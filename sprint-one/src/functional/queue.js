679aa02ed3fc308546b62855c46d990161190cc2
James <qjameso@berkeley.edu>
2022-02-24 16:16:51 -0800

    completed functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,37 @@
-var Queue = function() {
+var Queue = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var size = 0;
+
   // Implement the methods below
-
-  someInstance.enqueue = function(value) {
+  someInstance.enqueue = function (value) {
+    size++;
+    storage[size] = value;
   };
 
-  someInstance.dequeue = function() {
+  someInstance.dequeue = function () {
+    if (size === 0) {
+      return;
+    }
+
+    var dequeued = storage[1];
+    delete storage[1];
+    size--;
+
+    for (var key in storage) {
+      var value = storage[key];
+      delete storage[key];
+      storage[Number(key) - 1] = value;
+    }
+
+    return dequeued;
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,30 @@
-var Stack = function() {
+var Stack = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var size = 0;
+
   // Implement the methods below
-  someInstance.push = function(value) {
+  someInstance.push = function (value) {
+    size++;
+    storage[size] = value;
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function () {
+    if (size === 0) {
+      return;
+    }
+
+    var popped = storage[size];
+    delete storage[size];
+    size--;
+    return popped;
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    return size;
   };
 
   return someInstance;
