5042426d239fed56ffad4befa5de1a0874314f4a
James <qjameso@berkeley.edu>
2022-02-25 11:51:18 -0800

    completed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,21 +1,59 @@
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
+  list.addToTail = function (value) {
+    var currentNode = Node(value);
+    if (list.head === null) {
+      list.head = currentNode;
+      list.tail = currentNode;
+    }
+    if (list.head && list.tail) {
+      var prevNode = list.tail;
+      prevNode.next = currentNode;
+      list.tail = currentNode;
+    }
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
+    if (!list.head) {
+      return;
+    }
+
+    var currHeadVal = list.head.value;
+    var nextNode = list.head.next;
+    console.log(list.head, nextNode);
+
+    if (!nextNode) {
+      list.head = null;
+      list.tail = null;
+    } else {
+      list.head = nextNode;
+    }
+    return currHeadVal;
   };
 
-  list.contains = function(target) {
+  list.contains = function (target) {
+    var currNode = list.head;
+    if (currNode.value === target) {
+      return true;
+    }
+
+    while (currNode.next) {
+      currNode = currNode.next;
+      if (currNode.value === target) {
+        return true;
+      }
+    }
+
+    return false;
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;
