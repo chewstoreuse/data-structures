96ed32187ab76ce9fc301ebb6f14703f2812d003
James <qjameso@berkeley.edu>
2022-02-25 21:13:58 -0800

    completed graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,39 +1,62 @@
 
 
 // Instantiate a new graph
-var Graph = function() {
+var Graph = function () {
+  this.storage = {};
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
+Graph.prototype.addNode = function (node) {
+  this.storage[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
+Graph.prototype.contains = function (node) {
+  if (this.storage[node] !== undefined) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
+Graph.prototype.removeNode = function (node) {
+  for (key in this.storage) {
+    var nodeIndex = this.storage[key].indexOf(node);
+    if (nodeIndex !== -1) {
+      this.storage[key].splice(nodeIndex, 1);
+    }
+  }
+
+  delete this.storage[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
-Graph.prototype.hasEdge = function(fromNode, toNode) {
+Graph.prototype.hasEdge = function (fromNode, toNode) {
+  var fromArray = this.storage[fromNode];
+  var toArray = this.storage[toNode];
+
+  return (fromArray.indexOf(toNode) !== -1 && toArray.indexOf(fromNode) !== -1);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
-Graph.prototype.addEdge = function(fromNode, toNode) {
+Graph.prototype.addEdge = function (fromNode, toNode) {
+  this.storage[fromNode].push(toNode);
+  this.storage[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
-Graph.prototype.removeEdge = function(fromNode, toNode) {
+Graph.prototype.removeEdge = function (fromNode, toNode) {
+  this.storage[fromNode] = this.storage[fromNode].filter(function (element) { element !== toNode; });
 };
 
 // Pass in a callback which will be executed on each node of the graph.
-Graph.prototype.forEachNode = function(cb) {
+Graph.prototype.forEachNode = function (cb) {
+  for (var key in this.storage) {
+    cb(Number(key));
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
