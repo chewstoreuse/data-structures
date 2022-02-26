da8a367514145a246fa4a5b85e8e23144a5b5652
James <qjameso@berkeley.edu>
2022-02-25 21:14:27 -0800

    completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,38 @@
-var Set = function() {
+var Set = function () {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];// [] // fix me
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function (item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
+  // if (!this.contains(item)) {
+  //   this._storage[item] = item;
+  // }
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function (item) {
+  if (this._storage.indexOf(item) !== -1) {
+    return true;
+  }
+  return false;
+
+  // if (!this._storage[item]) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function (item) {
+  var indexRemove = this._storage.indexOf(item);
+  this._storage.splice(indexRemove, 1);
+
+  // delete this._storage[item];
 };
 
 /*
