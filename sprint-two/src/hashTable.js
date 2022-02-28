04ca28e8a608cd7ac2de786afc844e1aba6ef34a
James <qjameso@berkeley.edu>
2022-02-27 17:51:57 -0800

    determined time complexity of hashTable functions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,55 @@
 
 
-var HashTable = function() {
+var HashTable = function () {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function (k, v) {
+  var index = getIndexBelowMaxForKey(k, this._limit); // <---- linear
+
+  if (!this._storage.get(index)) {
+    this._storage.set(index, []);
+  }
+
+  this._storage.get(index).push([k, v]);
+
+  if (this._storage.get(index)) {
+    this._storage.get(index).forEach(function (tuple) {
+      if (tuple[0] === k) {
+        tuple[1] = v;
+        return;
+      }
+    });
+  }
 };
 
-HashTable.prototype.retrieve = function(k) {
+HashTable.prototype.retrieve = function (k) {
+  var retrievedValue;
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucketArray = this._storage.get(index);
+
+  bucketArray.forEach(function (tuple) {
+    if (tuple[0] === k) {
+      retrievedValue = tuple[1];
+    }
+  });
+
+  return retrievedValue;
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var target = this.retrieve(k);
+
+  var indexSplice;
+
+  this._storage.get(index).forEach(function (tuple, i) {
+    if (tuple[0] === k) {
+      var indexSplice = i;
+    }
+  });
+  this._storage.get(index).splice(indexSplice, 1);
 };
 
 
@@ -23,4 +58,7 @@ HashTable.prototype.remove = function(k) {
  * Complexity: What is the time complexity of the above functions?
  */
 
+//insert() - O(n)
+//retrieve() - O(n)
+//remove() - O(n)
 
